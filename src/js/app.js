8553ace29c320798dca7766491026407917c08c5
oleh <pshyk.oleg@gmail.com>
2021-10-12 01:36:00 +0300

    slider 2

--- src/js/app.js
+++ src/js/app.js
@@ -29,7 +29,6 @@ document.addEventListener('DOMContentLoaded', () => {
     arrows: false,
     autoplay: true,
     dots: true,
-    centerMode: false,
     autoplaySpeed: 10000,
     respondTo: 'window',    
     initialSlide: 0,
@@ -57,6 +56,38 @@ document.addEventListener('DOMContentLoaded', () => {
     ] 
   })
   $('.js-notable-slider').css({"opacity": "1","transition": "opacity 0.5s ease"});
+
+  $('.js-trending-slider').slick({
+    arrows: false,
+    autoplay: true,
+    dots: true,
+    autoplaySpeed: 10000,
+    respondTo: 'window',    
+    initialSlide: 0,
+    infinite: true,
+    nextArrow: '.next-trending-slick-btn',
+    prevArrow: '.previous-trending-slick-btn',
+    slidesToShow: 1,
+    slidesToScroll: 1,   
+    mobileFirst: true,    
+    responsive: [
+      {
+        breakpoint: 815,
+        settings: {
+          arrows: true, 
+          slidesToShow: 2,  
+        }
+      },
+      {
+        breakpoint: 1199,
+        settings: {      
+          arrows: true,    
+          slidesToShow: 3,  
+        }
+      }
+    ] 
+  })
+  $('.js-trending-slider').css({"opacity": "1","transition": "opacity 0.5s ease"});
 
 });
 if (module.hot) {
